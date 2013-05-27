72b761bdb2641b72741c2ecb54aefbad288bf9f9
Eric McCorkle <emc2@metricspace.net>
2013-05-26 23:27:44 -0400

    Fixed compile errors.

--- Control/Monad/LLVM/LLVMBuilder/Class.hs
+++ Control/Monad/LLVM/LLVMBuilder/Class.hs
@@ -35,76 +35,77 @@ module Control.Monad.LLVM.LLVMBuilder.Class(
        ) where
 
 import Control.Monad.Trans
-import LLVM.Core(ContextRef, ValueRef, TypeRef)
+import LLVM.Core(ValueRef, TypeRef, BasicBlockRef,
+                 Opcode, IntPredicate, RealPredicate)
 
 -- | Class for monads that carry LLVM context information
 class MonadIO m => MonadLLVMBuilder m where
-  positionBuilder :: BasicBlockRef -> ValueRef -> IO ()
-  positionBefore :: ValueRef -> IO ()
-  positionAtEnd :: BasicBlockRef -> IO ()
-  getInsertBlock :: IO BasicBlockRef
-  clearInsertionPosition :: IO ()
-  insertIntoBuilder :: ValueRef -> IO ()
-  insertIntoBuilderWithName :: ValueRef -> String -> IO ()
-  getCurrentDebugLocation :: IO ValueRef
-  setCurrentDebugLocation :: ValueRef -> IO ()
-  setInstDebugLocation :: ValueRef -> IO ()
-  buildRetVoid :: IO ValueRef
-  buildRet :: ValueRef -> IO ValueRef
-  buildAggregateRet :: [ValueRef] -> IO ValueRef
-  buildBr :: BasicBlockRef -> IO ValueRef
-  buildCondBr :: ValueRef -> BasicBlockRef -> BasicBlockRef -> IO ValueRef
-  buildSwitch :: Integral n => ValueRef -> BasicBlockRef -> n -> IO ValueRef
-  buildIndirectBr :: Integral n => ValueRef -> n -> IO ValueRef
+  positionBuilder :: BasicBlockRef -> ValueRef -> m ()
+  positionBefore :: ValueRef -> m ()
+  positionAtEnd :: BasicBlockRef -> m ()
+  getInsertBlock :: m BasicBlockRef
+  clearInsertionPosition :: m ()
+  insertIntoBuilder :: ValueRef -> m ()
+  insertIntoBuilderWithName :: ValueRef -> String -> m ()
+  getCurrentDebugLocation :: m ValueRef
+  setCurrentDebugLocation :: ValueRef -> m ()
+  setInstDebugLocation :: ValueRef -> m ()
+  buildRetVoid :: m ValueRef
+  buildRet :: ValueRef -> m ValueRef
+  buildAggregateRet :: [ValueRef] -> m ValueRef
+  buildBr :: BasicBlockRef -> m ValueRef
+  buildCondBr :: ValueRef -> BasicBlockRef -> BasicBlockRef -> m ValueRef
+  buildSwitch :: Integral n => ValueRef -> BasicBlockRef -> n -> m ValueRef
+  buildIndirectBr :: Integral n => ValueRef -> n -> m ValueRef
   buildInvoke :: ValueRef -> [ValueRef] -> BasicBlockRef ->
-                 BasicBlockRef -> String -> IO ValueRef
+                 BasicBlockRef -> String -> m ValueRef
   buildLandingPad :: Integral n =>
-                     TypeRef -> ValueRef -> n -> String -> IO ValueRef
-  buildResume :: ValueRef -> IO ValueRef
-  buildUnreachable :: IO ValueRef
-  buildAdd :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildNSWAdd :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildNUWAdd :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildFAdd :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildSub :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildNSWSub :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildNUWSub :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildFSub :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildMul :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildNSWMul :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildNUWMul :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildFMul :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildUDiv :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildSDiv :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildExactSDiv :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildFDiv :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildURem :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildSRem :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildFRem :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildShl :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildAShr :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildLShr :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildAnd :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildOr :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildXor :: ValueRef -> ValueRef -> String -> IO ValueRef
-  buildBinOp :: Opcode -> ValueRef -> ValueRef -> String -> IO ValueRef
-  buildNeg :: ValueRef -> String -> IO ValueRef
-  buildNSWNeg :: ValueRef -> String -> IO ValueRef
-  buildNUWNeg :: ValueRef -> String -> IO ValueRef
-  buildFNeg :: ValueRef -> String -> IO ValueRef
-  buildNot :: ValueRef -> String -> IO ValueRef
-  buildMalloc :: TypeRef -> String -> IO ValueRef
-  buildArrayMalloc :: TypeRef -> ValueRef -> String -> IO ValueRef
-  buildAlloca :: TypeRef -> String -> IO ValueRef
-  buildArrayAlloca :: TypeRef -> ValueRef -> String -> IO ValueRef
-  buildFree :: ValueRef -> IO ValueRef
-  buildLoad :: ValueRef -> String -> IO ValueRef
+                     TypeRef -> ValueRef -> n -> String -> m ValueRef
+  buildResume :: ValueRef -> m ValueRef
+  buildUnreachable :: m ValueRef
+  buildAdd :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildNSWAdd :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildNUWAdd :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildFAdd :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildSub :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildNSWSub :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildNUWSub :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildFSub :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildMul :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildNSWMul :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildNUWMul :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildFMul :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildUDiv :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildSDiv :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildExactSDiv :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildFDiv :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildURem :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildSRem :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildFRem :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildShl :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildAShr :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildLShr :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildAnd :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildOr :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildXor :: ValueRef -> ValueRef -> String -> m ValueRef
+  buildBinOp :: Opcode -> ValueRef -> ValueRef -> String -> m ValueRef
+  buildNeg :: ValueRef -> String -> m ValueRef
+  buildNSWNeg :: ValueRef -> String -> m ValueRef
+  buildNUWNeg :: ValueRef -> String -> m ValueRef
+  buildFNeg :: ValueRef -> String -> m ValueRef
+  buildNot :: ValueRef -> String -> m ValueRef
+  buildMalloc :: TypeRef -> String -> m ValueRef
+  buildArrayMalloc :: TypeRef -> ValueRef -> String -> m ValueRef
+  buildAlloca :: TypeRef -> String -> m ValueRef
+  buildArrayAlloca :: TypeRef -> ValueRef -> String -> m ValueRef
+  buildFree :: ValueRef -> m ValueRef
+  buildLoad :: ValueRef -> String -> m ValueRef
 
   buildStore :: ValueRef
              -- ^ Value
              -> ValueRef
              -- ^ Pointer value
-             -> IO ValueRef
+             -> m ValueRef
              -- ^ Store instruction
 
   buildGEP :: ValueRef
@@ -113,7 +114,7 @@ class MonadIO m => MonadLLVMBuilder m where
            -- ^ Indexes
            -> String
            -- ^ Name
-           -> IO ValueRef
+           -> m ValueRef
 
   buildInBoundsGEP :: ValueRef
                    -- ^ Pointer value
@@ -121,49 +122,49 @@ class MonadIO m => MonadLLVMBuilder m where
                    -- ^ Indexes
                    -> String
                    -- ^ Name
-                   -> IO ValueRef
+                   -> m ValueRef
 
-  buildStructGEP :: Integral i => ValueRef -> i -> String -> IO ValueRef
-  buildGlobalString :: String -> String -> IO ValueRef
-  buildGlobalStringPtr :: String -> String -> IO ValueRef
-  buildTrunc :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildZExt :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildSExt :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildFPToUI :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildFPToSI :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildUIToFP :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildSIToFP :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildFPTrunc :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildFPExt :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildPtrToInt :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildIntToPtr :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildBitCast :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildZExtOrBitCast :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildSExtOrBitCast :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildTruncOrBitCast :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildCast :: Opcode -> ValueRef -> TypeRef -> String -> IO ValueRef
-  buildPointerCast :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildIntCast :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildFPCast :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildICmp :: IntPredicate -> ValueRef -> ValueRef -> String -> IO ValueRef
-  buildFCmp :: RealPredicate -> ValueRef -> ValueRef -> String -> IO ValueRef
-  buildPhi :: TypeRef -> String -> IO ValueRef
-  buildCall :: ValueRef -> [ValueRef] -> String -> IO ValueRef
-  buildSelect :: ValueRef -> ValueRef -> ValueRef -> String -> IO ValueRef
-  buildVAArg :: ValueRef -> TypeRef -> String -> IO ValueRef
-  buildExtractElement :: ValueRef -> ValueRef -> String -> IO ValueRef
+  buildStructGEP :: Integral i => ValueRef -> i -> String -> m ValueRef
+  buildGlobalString :: String -> String -> m ValueRef
+  buildGlobalStringPtr :: String -> String -> m ValueRef
+  buildTrunc :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildZExt :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildSExt :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildFPToUI :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildFPToSI :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildUIToFP :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildSIToFP :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildFPTrunc :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildFPExt :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildPtrToInt :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildIntToPtr :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildBitCast :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildZExtOrBitCast :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildSExtOrBitCast :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildTruncOrBitCast :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildCast :: Opcode -> ValueRef -> TypeRef -> String -> m ValueRef
+  buildPointerCast :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildIntCast :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildFPCast :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildICmp :: IntPredicate -> ValueRef -> ValueRef -> String -> m ValueRef
+  buildFCmp :: RealPredicate -> ValueRef -> ValueRef -> String -> m ValueRef
+  buildPhi :: TypeRef -> String -> m ValueRef
+  buildCall :: ValueRef -> [ValueRef] -> String -> m ValueRef
+  buildSelect :: ValueRef -> ValueRef -> ValueRef -> String -> m ValueRef
+  buildVAArg :: ValueRef -> TypeRef -> String -> m ValueRef
+  buildExtractElement :: ValueRef -> ValueRef -> String -> m ValueRef
 
   buildInsertElement :: ValueRef -> ValueRef -> ValueRef -> String ->
-                        IO ValueRef
+                        m ValueRef
 
   buildShuffleVector :: ValueRef -> ValueRef -> ValueRef -> String ->
-                        IO ValueRef
+                        m ValueRef
 
-  buildExtractValue :: Integral n => ValueRef -> n -> String -> IO ValueRef
+  buildExtractValue :: Integral n => ValueRef -> n -> String -> m ValueRef
 
   buildInsertValue :: Integral n =>
-                      ValueRef -> ValueRef -> n -> String -> IO ValueRef
+                      ValueRef -> ValueRef -> n -> String -> m ValueRef
 
-  buildIsNull :: ValueRef -> String -> IO ValueRef
-  buildIsNotNull :: ValueRef -> String -> IO ValueRef
-  buildPtrDiff :: ValueRef -> ValueRef -> String -> IO ValueRef
+  buildIsNull :: ValueRef -> String -> m ValueRef
+  buildIsNotNull :: ValueRef -> String -> m ValueRef
+  buildPtrDiff :: ValueRef -> ValueRef -> String -> m ValueRef
