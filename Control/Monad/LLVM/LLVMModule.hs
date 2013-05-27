01b00521c375145a5fa3886673e7d8278f0d673a
Eric McCorkle <emc2@metricspace.net>
2013-05-26 22:38:27 -0400

    Completed LLVM module monad and monad transformers.

--- Control/Monad/LLVM/LLVMModule.hs
+++ Control/Monad/LLVM/LLVMModule.hs
@@ -34,10 +34,18 @@
 module Control.Monad.LLVM.LLVMModule(
        MonadLLVMModule(..),
        LLVMModuleT,
-       LLVMModule
+       LLVMModule,
+       runWithModule,
+       runWithNewModule,
+       runWithNewModuleInContext
        ) where
 
+import Control.Applicative
+import Control.Monad.Cont.Class
+import Control.Monad.Error.Class
+import Control.Monad.LLVM.LLVMContext.Class
 import Control.Monad.LLVM.LLVMModule.Class
+import Control.Monad.Writer.Class
 import Control.Monad.Reader
 import Control.Monad.State
 import Prelude hiding (mod)
@@ -47,6 +55,34 @@ import qualified LLVM.Core as LLVM
 newtype LLVMModuleT m a =
   LLVMModuleT { unpackLLVMModuleT :: ReaderT LLVM.ModuleRef m a }
 type LLVMModule a = LLVMModuleT IO a
+
+-- | Run an LLVMModuleT with a given module.
+runWithModule :: MonadIO m => LLVMModuleT m a -> LLVM.ModuleRef -> m a
+runWithModule m = runReaderT (unpackLLVMModuleT m)
+
+-- | Create a new module with a given name and use it to run an
+-- LLVMModuleT.  The created module is disposed at the end, so the
+-- result value must not depend on its continued existence.
+runWithNewModule :: MonadIO m => LLVMModuleT m a -> String -> m a
+runWithNewModule m name =
+  do
+    mod <- liftIO (LLVM.moduleCreateWithName name)
+    res <- runReaderT (unpackLLVMModuleT m) mod
+    liftIO (LLVM.disposeModule mod)
+    return res
+
+-- | Create a new module with a given name in the context contained by
+-- an LLVMContextRefT and use it to run an LLVMModuleT.  The created
+-- module is disposed at the end, so the result value must not depend
+-- on its continued existence.
+runWithNewModuleInContext :: (MonadLLVMContext m, MonadIO m) =>
+                             LLVMModuleT m a -> String -> m a
+runWithNewModuleInContext m name =
+  do
+    mod <- moduleCreateWithNameInContext name
+    res <- runReaderT (unpackLLVMModuleT m) mod
+    liftIO (LLVM.disposeModule mod)
+    return res
 
 getDataLayout' :: MonadIO m => (ReaderT LLVM.ModuleRef m) String
 getDataLayout' = ask >>= liftIO . LLVM.getDataLayout
@@ -196,3 +232,64 @@ instance MonadTrans LLVMModuleT where
 instance MonadState s m => MonadState s (LLVMModuleT m) where
   get = lift get
   put = lift . put
+
+instance MonadLLVMContext m => MonadLLVMContext (LLVMModuleT m) where
+  getMDKindIDInContext = lift . getMDKindIDInContext
+  moduleCreateWithNameInContext = lift . moduleCreateWithNameInContext
+  int1TypeInContext = lift int1TypeInContext
+  int8TypeInContext = lift int8TypeInContext
+  int16TypeInContext = lift int16TypeInContext
+  int32TypeInContext = lift int32TypeInContext
+  int64TypeInContext = lift int64TypeInContext
+  intTypeInContext = lift . intTypeInContext
+  floatTypeInContext = lift floatTypeInContext
+  doubleTypeInContext = lift doubleTypeInContext
+  x86FP80TypeInContext = lift x86FP80TypeInContext
+  fp128TypeInContext = lift fp128TypeInContext
+  ppcFP128TypeInContext = lift ppcFP128TypeInContext
+  voidTypeInContext = lift voidTypeInContext
+  labelTypeInContext = lift labelTypeInContext
+  x86MMXTypeInContext = lift x86MMXTypeInContext
+  structTypeInContext fields = lift . structTypeInContext fields
+  structCreateNamed = lift . structCreateNamed
+  constStringInContext name = lift . constStringInContext name
+  constStructInContext fields = lift . constStructInContext fields
+  mdStringInContext = lift . mdStringInContext
+  mdNodeInContext = lift . mdNodeInContext
+  appendBasicBlockInContext block = lift . appendBasicBlockInContext block
+  insertBasicBlockInContext block = lift . insertBasicBlockInContext block
+  createBuilderInContext = lift createBuilderInContext
+
+instance Functor m => Functor (LLVMModuleT m) where
+  fmap f  = LLVMModuleT . mapReaderT (fmap f) . unpackLLVMModuleT
+
+instance Applicative m => Applicative (LLVMModuleT m) where
+  pure = LLVMModuleT . pure
+  f <*> v = LLVMModuleT $ unpackLLVMModuleT f <*> unpackLLVMModuleT v
+
+instance Alternative m => Alternative (LLVMModuleT m) where
+  empty = LLVMModuleT empty
+  m <|> n = LLVMModuleT $ unpackLLVMModuleT m <|> unpackLLVMModuleT n
+
+instance MonadPlus m => MonadPlus (LLVMModuleT m) where
+  mzero = lift mzero
+  m `mplus` n = LLVMModuleT $ unpackLLVMModuleT m `mplus` unpackLLVMModuleT n
+
+instance MonadFix m => MonadFix (LLVMModuleT m) where
+  mfix f = LLVMModuleT $ mfix $ (\a -> unpackLLVMModuleT (f a))
+
+instance MonadCont m => MonadCont (LLVMModuleT m) where
+  callCC f =
+    LLVMModuleT $ callCC $
+      (\c -> unpackLLVMModuleT (f (\a -> LLVMModuleT $ c a)))
+
+instance MonadError e m => MonadError e (LLVMModuleT m) where
+  throwError = lift . throwError
+  m `catchError` h =
+    LLVMModuleT $ unpackLLVMModuleT m `catchError`
+                    (\e -> unpackLLVMModuleT (h e))
+
+instance MonadWriter w m => MonadWriter w (LLVMModuleT m) where
+  tell = lift . tell
+  listen m = LLVMModuleT $ listen (unpackLLVMModuleT m)
+  pass m = LLVMModuleT $ pass (unpackLLVMModuleT m)
